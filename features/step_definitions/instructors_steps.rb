3ef8aa08314a1cd02bc2fa409915b6a29ae05d17
databurj <17100189@lums.edu.pk>
2016-03-22 12:24:22 +0000

    front end bug fixed

--- features/step_definitions/instructors_steps.rb
+++ features/step_definitions/instructors_steps.rb
@@ -5,3 +5,13 @@ Given(/^the following instructors exist:$/) do |instructors_table|
       Instructor.create!(instructor)
   end
 end
+
+
+When(/^I follow "([^"]*)"$/) do |arg1|
+  # pending # Write code here that turns the phrase above into concrete actions
+  click_link(arg1)
+end
+
+# When(/^I press "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
